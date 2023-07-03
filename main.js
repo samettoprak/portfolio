a575d8b9585779c5feca24c4224dbea017fb0223
samettoprak <topraksamet41@gmail.com>
2023-07-03 22:05:46 +0300

    Created circle animation

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ import "/components/header.css";
 import "/components/hero.css";
 import "/components/about.css";
 import "/components/projects.css";
+import "/components/contact.css";
+import "/components/circle.css";
 
 import "/utils.css";
