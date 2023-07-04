236220649cbd4d2852392a18858997efd8e414b6
samettoprak <topraksamet41@gmail.com>
2023-07-04 22:44:54 +0300

    Made it more responsive

--- app.js
+++ app.js
@@ -12,15 +12,3 @@ const observer = new IntersectionObserver((entries) => {
 progressBar.forEach((progress) => {
   observer.observe(progress);
 });
-
-// const progressBar = document.querySelectorAll(".bar");
-// const observer = new IntersectionObserver((entries) => {
-//   console.log(entries);
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       progressBar.classList.add("fill-animation");
-//     }
-//   });
-// });
-
-// observer.observe(document.querySelector(".bar__container"));
