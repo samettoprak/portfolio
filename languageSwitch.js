0542c4a22db5378c99920a69472e318a0f6256e5
samettoprak <topraksamet41@gmail.com>
2023-07-06 00:41:04 +0300

    made it more responsive

--- languageSwitch.js
+++ languageSwitch.js
@@ -26,6 +26,8 @@ project__title4 = document.getElementById("project__title4");
 project__desc4 = document.getElementById("project__desc4");
 project__title5 = document.getElementById("project__title5");
 project__desc5 = document.getElementById("project__desc5");
+project__title6 = document.getElementById("project__title6");
+project__desc6 = document.getElementById("project__desc6");
 
 project__process = document.querySelectorAll(".project__process");
 contact_id = document.getElementById("contact_id");
@@ -49,14 +51,15 @@ document.getElementById("language__btn").onclick = () => {
     li4.innerHTML = "İletişim";
     li5.innerHTML = "değiştir";
     hero__hi.innerHTML = "Merhaba! Ben Samet.";
-    hero__description.innerHTML = "bişiler";
+    hero__description.innerHTML =
+      "<strong> Portfolyo </strong> web sayfama hoşgeldin.";
     about__title.innerHTML = "Hakkımda";
     about__desc1.innerHTML =
-      "Selam, Ben Samet Toprak. Trakya Üniversitesi'nden yeni mezun olmuş, 22 yaşında bir Front-End Web geliştiricisiyim.";
+      "Selam, Ben Samet Toprak. Trakya Üniversitesi'nden yeni mezun olmuş, 22 yaşında bir <strong> Front-End Web </strong> geliştiricisiyim.";
     about__desc2.innerHTML =
       "Tasarım ve kullanıcı deneyimi odaklı olarak, Front-End Web geliştirme konusunda sağlam bir arkaplana sahibim.";
     about__desc3.innerHTML =
-      "HTML, CSS ve JavaScript konularında yetenekliyim ve React.js gibi popüler frameworkler kullanarak dinamik ve etkileşimli web uygulamaları oluşturuyorum. Heyecan verici fırsatlar için bana gönül rahatlığıyla ulaşabilirsiniz. Teşekkürler.";
+      "<strong> HTML</strong> , <strong>CSS </strong>  ve <strong>JavaScript </strong>  konularında yetenekliyim ve <strong>ReactJS</strong>  gibi popüler frameworkler kullanarak dinamik ve etkileşimli web uygulamaları oluşturuyorum. Heyecan verici fırsatlar için bana gönül rahatlığıyla ulaşabilirsiniz. Teşekkürler.";
     about__techs.innerHTML = "Teknolojiler";
     projects__title.innerHTML = "PROJELER";
 
@@ -76,6 +79,9 @@ document.getElementById("language__btn").onclick = () => {
     project__title5.innerHTML = "WhatsApp REST API";
     project__desc5.innerHTML =
       "WhatsApp için REST API backend.<br/> API, mesaj gönderme, grup yönetimi ve kullanıcı yönetimi gibi temel işlevleri destekler. Veri alışverişini JSON formatında yapar ve HTTP protokolü üzerinden çağrı yapar.<br/> Java ile programlandı.";
+    project__title6.innerHTML = "E-Ticaret Uygulaması";
+    project__desc6.innerHTML =
+      "Bu e-ticaret mobil uyguması, kullanıcıların ürünleri görüntüleyip satın alabileceği bir platformdur. <br/> Güncel ürün verilerini API üzerinden alarak kullanıcılara gerçek zamanlı bilgi sunar. Kullanıcılar alışveriş sepetlerini yönetebilir ve indirimlerden haberdar olabilirler. ";
     project__process.forEach((e) => {
       e.innerHTML = "Proje durumu";
     });
@@ -94,14 +100,15 @@ document.getElementById("language__btn").onclick = () => {
     li4.innerHTML = "Contact";
     li5.innerHTML = "Resume";
     hero__hi.innerHTML = "Hi! I'm Samet.";
-    hero__description.innerHTML = "lorem xd";
+    hero__description.innerHTML =
+      "Welcome to my <strong> portfolio </strong> web page.";
     about__title.innerHTML = "About Me";
     about__desc1.innerHTML =
-      "Hello, I'm Samet Toprak, a 22 year old Front-End Web Developer who recently graduated from Trakya University.";
+      "Hello, I'm Samet Toprak, a 22 year old <strong> Front-End Web</strong> Developer who recently graduated from Trakya University.";
     about__desc2.innerHTML =
       "I have a strong background in front-end web development, with a focus on design and user experience.";
     about__desc3.innerHTML =
-      "Skilled in HTML, CSS, and JavaScript, I create dynamic and interactive web applications using popular frameworks like React.js. Feel free to reach out to me for exciting opportunities. Thank you.";
+      "Skilled in <strong>HTML</strong> ,<strong>CSS</strong> , and <strong>JavaScript</strong> , I create dynamic and interactive web applications using popular frameworks like <strong>ReactJS</strong> . Feel free to reach out to me for exciting opportunities. Thank you.";
     about__techs.innerHTML = "Technologies";
     projects__title.innerHTML = "PROJECTS";
 
@@ -111,16 +118,19 @@ document.getElementById("language__btn").onclick = () => {
       "Turkish Sing Language Translator can detect hands & signs that means letters and words. <br /> The main purpose is to remove barriers of the people with disabilities in communication. <br /> Programmed with Python.";
     project__title2.innerHTML = "Online Quiz App";
     project__desc2.innerHTML =
-      "This web app offers an interactive learning experience designed for both students and teachers. <br /> Teachers can create quizzes, set deadlines, and track the grades of all students. On the other hand, students can take exams through this app and view their own grades. <br /> Programmed with ReactJS";
+      "This web app offers an interactive learning experience designed for both students and teachers. <br /> Teachers can create quizzes, set deadlines, and track the grades of all students. On the other hand, students can take exams through this app and view their own grades. <br /> Programmed with ReactJS.";
     project__title3.innerHTML = "Netflix Clone";
     project__desc3.innerHTML =
-      "Classic Netflix clone. <br /> Integrated authentication functionality with Firebase, allowing users to create accounts, sign in, and personalize their content. <br /> Programmed with ReactJS";
+      "Classic Netflix clone. <br /> Integrated authentication functionality with Firebase, allowing users to create accounts, sign in, and personalize their content. <br /> Programmed with ReactJS.";
     project__title4.innerHTML = "WhatsApp Clone";
     project__desc4.innerHTML =
-      "Classic WhatsApp clone.<br /> You can send message to peoples or groups & delete messages.<br /> Programmed with ReactJS";
+      "Classic WhatsApp clone.<br /> You can send message to peoples or groups & delete messages.<br /> Programmed with ReactJS.";
     project__title5.innerHTML = "WhatsApp REST API";
     project__desc5.innerHTML =
       "REST API for WhatsApp backend.<br /> The API supports basic functionalities such as sending messages, managing groups, and user administration. It exchanges data in JSON format and makes calls over the HTTP protocol.<br/> Programmed with Java.";
+    project__title6.innerHTML = "E-Commerce App";
+    project__desc6.innerHTML =
+      "This e-commerce mobile application is a platform where users can view and purchase various products. <br /> It retrieves real-time product data through an external API and presents it to the users. Users can create accounts, manage their shopping carts, and stay informed about discounts.<br /> Programmed with React Native.";
     project__process.forEach((e) => {
       e.innerHTML = "Project Process";
     });
