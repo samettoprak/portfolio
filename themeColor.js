a860f46e6e243672d10d8103cd404399cb41b227
samettoprak <topraksamet41@gmail.com>
2023-07-05 03:18:10 +0300

    Theme Update

--- themeColor.js
+++ themeColor.js
@@ -1,5 +1,8 @@
 var darkTheme = true;
+const slate400 = "var(--clr-slate400)";
+const slate600 = "var(--clr-slate600)";
 const slate800 = "var(--clr-slate800)";
+const slateLight = "var(--clr-light)";
 
 aboutMeTitle = document.querySelector(".about__title");
 projectsTitle = document.querySelector(".projects__title");
@@ -10,12 +13,13 @@ contacts = document.querySelectorAll(".contact__img");
 document.getElementById("themeBtn").onclick = () => {
   console.log("samet");
   if (darkTheme) {
-    document.body.style.background = "white";
-    aboutMeTitle.style.color = slate800;
-    projectsTitle.style.color = slate800;
     darkTheme = false;
+    document.body.style.background = "white";
+    aboutMeTitle.style.color = slate600;
+    projectsTitle.style.color = slate600;
+
     allProjectTitle.forEach((title) => {
-      title.style.color = slate800;
+      title.style.color = slate600;
     });
     githubImage.forEach((img) => {
       img.style.filter = "invert(0)";
@@ -24,7 +28,18 @@ document.getElementById("themeBtn").onclick = () => {
       contact.style.filter = "invert(0)";
     });
   } else {
-    document.body.style.background = "var(--clr-dark)";
     darkTheme = true;
+    document.body.style.background = "var(--clr-dark)";
+    aboutMeTitle.style.color = slateLight;
+    projectsTitle.style.color = slateLight;
+    allProjectTitle.forEach((title) => {
+      title.style.color = slate400;
+    });
+    githubImage.forEach((img) => {
+      img.style.filter = "invert()";
+    });
+    contacts.forEach((contact) => {
+      contact.style.filter = "invert()";
+    });
   }
 };
